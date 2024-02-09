d3a2e16b303cb90f10770a831e6fbe3095939146
Vidhya-08 <140006223+Vidhya-08@users.noreply.github.com>
2024-02-09 21:27:43 +0530

    Add files via upload

---
+++ sttext.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("I am Title")
+st.header("I am Header")
+st.subheader("I am sub header")
+st.markdown("I am markdown")
+st.code("I am a code block. Eg - int a=5")
+st.text("I am Text")
+st.write("I am write")
