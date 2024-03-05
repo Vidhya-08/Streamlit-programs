49114340b28ea34ceed0db56794461f6a894e721
Vidhya-08 <140006223+Vidhya-08@users.noreply.github.com>
2024-03-05 19:11:30 +0530

    Add files via upload

---
+++ stdataframe.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.write("This is a dataframe demo")
+df=pd.DataFrame(
+    np.random.randn(50,20),
+    columns=('col %d' % i for i in range(20)))
+st.dataframe(df)
